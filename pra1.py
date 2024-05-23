539848ad09d22a1394c0724e8953d82e291a4b78
isheilachuang <160543838+isheilachuang@users.noreply.github.com>
2024-05-24 06:49:20 +0800

    practice

---
+++ pra1.py
@@ -0,0 +1,17 @@
+var1 = 20
+var2 = 13
+
+if (var1 > var2):
+    print ('var1 is greater than var2')
+
+num1 = 10
+if (num1 % 2 == 0):
+    print(num1 ** 2)
+
+a = float(input('Enter an integer: '))
+if a % 2 == 0:
+    print(f'{a} is an even number')
+else:
+    print(f'{a} is an odd number')
+
+
