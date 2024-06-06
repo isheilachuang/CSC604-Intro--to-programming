723081f85b7d7c09f207c60da9edf28a50edf9d7
isheilachuang <160543838+isheilachuang@users.noreply.github.com>
2024-06-07 05:29:56 +0800

    0604

---
+++ 0604ex1.py
@@ -0,0 +1,13 @@
+
+A = 3
+B = 5
+C = 7
+D = 9
+
+A, B, C, D = D, B, A, C
+
+
+print("A =", A)  # A = 9
+print("B =", B)  # B = 5
+print("C =", C)  # C = 3
+print("D =", D)  # D = 7
---
+++ 0604ex2.py
@@ -0,0 +1,12 @@
+
+div = {i: True for i in range(1, 101) if i % 5 == 0}
+
+print(div)
+
+dict1 = {}
+
+for num in range, (1, 101):
+    if num % 5 == 0:
+        dict1[num] = num ** 3
+
+print(dict1)
